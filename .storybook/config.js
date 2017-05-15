288896aac946a794ffa74fb1036ecd64504c1304
DESKTOP-F92KPNI\Francis Vidal <francisvidal@scolab.com>
2017-05-15 17:50:25 -0400

    Fix some bug, still no cigar.

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,7 +6,7 @@
 import { configure } from '@kadira/storybook';
 
 function loadStories() {
-  require('../src/stories/index.tsx');
+  require('../src/stories/index.jsx');
 }
 
 configure(loadStories, module);
