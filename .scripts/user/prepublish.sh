9bd4c38edaf4bb09813329343be7e5cabe20cf1c
Andre Lacasse <andrelacasse@scolab.com>
2017-05-26 11:05:56 -0400

    Change prepublish.sh script for adding images/css/fonts copying.

--- .scripts/user/prepublish.sh
+++ .scripts/user/prepublish.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 # Use this file to your own code to run at NPM `prepublish` event.
+
+cp -rf ./src/fonts/ ./dist/fonts/
+cp -rf ./src/components/images/ ./dist/components/images/
+cp -rf ./src/components/CanvasPIXI/images/ ./dist/components/CanvasPIXI/images/
+cp ./src/ExplodingDots.css ./dist/
+cp ./src/font-awesome.min.css ./dist/
+cp ./src/do_drag_blue.json ./dist/
+cp ./src/do_drag_red.json ./dist/
+cp ./src/dot_explode.json ./dist/
+cp ./src/dot_implode.json ./dist/
+
