7376b167f81cc19d8a5a7e937b51718fe55780c0
Francis Vidal <fvidal@hibernum.com>
2017-09-01 12:57:08 -0400

    Put a background color to see the buttons

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,11 @@
 // This is an auto generated file with React CDK.
 // Do not modify this file.
 
-import { configure } from '@kadira/storybook';
+// import { configure } from '@kadira/storybook';
+import { configure, addDecorator } from '@kadira/storybook';
+import backgroundColor from 'react-storybook-decorator-background';
+
+addDecorator(backgroundColor(['#F5F5F5']));
 
 function loadStories() {
   require('../src/stories/index.tsx');
